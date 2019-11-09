7eadc0abcbc3a4f503b3ab3bf8de3badfd4d7dd5
DarrenLUCreate <55569667+DarrenLUCreate@users.noreply.github.com>
2019-11-09 22:28:21 +0800

    Create merge_sort_06170239.py

---
+++ HW2/merge_sort_06170239.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def merge(self,array):
+    
+        if len(array)>1:
+            mid=len(array)//2 #設定切分點為list的中間
+            left=self.merge(array[:mid])#左邊的list是從頭到中間
+            right=self.merge(array[mid:])#右邊的list是從中間到最後
+        else:
+            return array #當list長度小於1時回傳list
+        
+        left_idx=0 #設定左邊INDEX初始值為0
+        right_idx=0#測定右邊INDEX初始值為0
+        result=[] #新的存放數值的list
+    
+        while left_idx<len(left) and right_idx<len(right):#當左右的INDEX都小於該對應的list長度後
+            if left[left_idx]<right[right_idx]: #比較兩邊的大小
+                result.append(left[left_idx]) #比較小的放入result裡
+                left_idx+=1 #左邊的INDEX往後推進1
+            else:
+                result.append(right[right_idx])
+                right_idx+=1
+            
+        while left_idx<len(left): #當只有左邊有剩下的值時，將剩餘的數值加入
+            result.append(left[left_idx])
+            left_idx+=1
+        
+        while right_idx<len(right):
+            result.append(right[right_idx])
+            right_idx+=1
+        
+        return result #回傳result
+
